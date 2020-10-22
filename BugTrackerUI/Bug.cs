b5c9e874036de1ac16efbe735a377accc12d4e8f
Jonas Rosberg <rojo19@student.ju.se>
2020-10-22 12:57:35 +0200

    Completed module

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -9,11 +9,12 @@ namespace BugTrackerUI
     public class Bug
     {
         public int Id { get; set; }
-
+        [Required]
         public string Title { get; set; }
-
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
-
+        [Range(1, 5)]
         public int Priority { get; set; }
     }
 }
