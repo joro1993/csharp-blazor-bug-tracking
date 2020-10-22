132f476d1fb812277bdc6f00e3db55d8d7923b99
Jonas Rosberg <rojo19@student.ju.se>
2020-10-22 13:02:23 +0200

    update

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -14,6 +14,7 @@ namespace BugTrackerUI
         [Required]
         [MinLength(10)]
         public string Description { get; set; }
+        [Required]
         [Range(1, 5)]
         public int Priority { get; set; }
     }
